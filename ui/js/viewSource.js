39363e5835ae73415f39ab79bc0c036eee73a9a6
sosim13 <sosim13p@gmail.com>
2018-04-26 11:14:59 +0900

    321

--- ui/js/viewSource.js
+++ ui/js/viewSource.js
@@ -16,3 +16,41 @@ if (!source) {
     
 agate.runScript("function.popupSource('"+  escapeFunctionScript(source) + "')");
 
+
+
+
+/*
+function escapeFunctionScript(text)
+{
+	text = text.replace(/\\/gm, "\\\\");
+	text = text.replace(/\n/gm, "\\n");
+	text = text.replace(/\r/gm, "\\r");
+	text = text.replace(/'/gm, "\\'");
+	
+	return text;	
+}
+
+var source =  document.all[0].outerHTML;
+
+if (!source) {
+	source =  "<html>\r\n" + document.all[0].innerHTML + "\r\n</html>";
+}
+    escapeFunctionScript(source) 
+	source.indexof("Æ÷Åä¼¥");
+agate.runScript("function.popupSource('"+  escapeFunctionScript(source) + "')");
+
+document.getElementsByTagName("title")["0"].innerText
+
+http://nikemania.com/bbs/bbs_list.php?bbs=popbbs4
+[101].attributes.id.ownerDocument.documentURI
+source.indexof("Æ÷Åä¼¥");
+
+
+
+var value = "º£³ð";
+source.indexOf(value);
+source.indexOf(value, source.indexOf(value)+1);
+if(source.indexOf(value) >= 0){
+	source.substr(source.indexOf(value), source.indexOf("<", source.indexOf(value))-source.indexOf(value)		);
+}
+*/
